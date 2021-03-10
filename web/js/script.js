99ef266ff53d4d03ff9b5b897856a352d06e62da
Guilherme Felicio <guifelicio7@gmail.com>
2021-03-10 15:21:43 -0300

    Adicionado animações de slides na página portfólio

---
+++ web/js/script.js
@@ -0,0 +1,12 @@
+if(window.SimpleSlide){
+new SimpleSlide({
+  slide: 'quote', // nome do atributo 
+  time: 5000 // tempo de transição dos slides
+});
+
+new SimpleSlide({
+  slide: 'portfolio', // nome do atributo 
+  nav: true,
+  time: 5000 // tempo de transição dos slides
+});
+}
